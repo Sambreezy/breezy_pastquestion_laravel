58d92a1b367477a5f784b6958c00e55ed28b2fed
bobbyaxe61 <bobbyaxe61@gmail.com>
2020-05-14 07:45:16 +0100

    Minor Test

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::group([
 
     // Auth Routes
     Route::post('register', 'AuthController@register');
-    Route::post('login', 'AuthController@login');
+    Route::all('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('forgot', 'AuthController@forgotPassword');
     Route::post('reset', 'AuthController@resetPassword');
