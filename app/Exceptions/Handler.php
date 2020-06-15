bb7c3a74924f7f56ebcadc8b9b2ec2c0f9e25c2d
bobbyaxe61 <bobbyaxe61@gmail.com>
2020-06-15 01:05:23 +0100

    Added Exception Handlers

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,10 +3,21 @@
 namespace App\Exceptions;
 
 use Exception;
+use App\Traits\ApiResponderTrait;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException as AuthenticationException;
+use Illuminate\Auth\Access\AuthorizationException as AuthorizationException;
+use Illuminate\Validation\ValidationException as ValidationException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException as MethodNotAllowed;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException as NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException as HttpException;
+use App\Exceptions\CustomException as CustomException;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponderTrait;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -29,10 +40,10 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
@@ -41,11 +52,57 @@ class Handler extends ExceptionHandler
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
+        // Get an ovsettings value
+        $api_exception_handler = config('ovsettings.api_exception_handler', false);
+
+        if ($request->expectsJson() && $api_exception_handler) {
+
+            // Thrown when an error occurs when a user makes an unauthenticated request
+            if ($exception instanceof AuthenticationException) {
+                return $this->authenticationFailure();
+            }
+
+            // Thrown when a user makes requests that Auth service does not validated
+            if ($exception instanceof AuthorizationException) {
+                return $this->forbiddenAccess();
+            }
+
+            // Thrown when the request fails Laravel FormValidator validation.
+            if ($exception instanceof ValidationException) {
+                return $this->formProcessingFailure($exception->errors(),'Inappropriate input');
+            }
+
+            // Thrown when HTTP Method is incorrect when requesting routing
+            if ($exception instanceof MethodNotAllowed) {
+                return $this->wrongRequestType($exception->getMessage());
+            }
+
+            // Thrown when the HTTP requested route can not be found
+            if ($exception instanceof NotFoundHttpException) {
+                return $this->notFound();
+            }
+
+            // Thrown when processing HTTP requests is unsuccessful
+            if ($exception instanceof HttpException) {
+                return $this->unavailableService();
+            }
+
+            // Thrown when a custom exception occurs.
+            if ($exception instanceof CustomException) {
+                return $this->internalServerError($exception->getMessage());
+            }
+
+            // Thrown when an exception occurs.
+            if ($exception instanceof Exception) {
+                return $this->internalServerError();
+            }
+        }
+
         return parent::render($request, $exception);
     }
 }
